62d1a446db207251a42c95ce101d982a9814ef84
T490s <batudal@gmail.com>
2022-04-21 16:27:15 +0300

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
+    mainnet: {
+      url: process.env.MAINNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     polygon: {
       url: process.env.POLYGON_URL || "",
       accounts:
