8411d4ae97d0fd8b8f1c2f937c507a37f811b94e
trizin <25263018+trizin@users.noreply.github.com>
2022-04-22 01:44:40 +0300

    5%

--- contracts/METActivists.sol
+++ contracts/METActivists.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract METActivists is ERC721A, Ownable {
-
     bytes32 public rootActivist;
     bytes32 public rootReserve;
     uint256 public immutable ACTIVIST_PRICE = 0.05 ether;
@@ -17,7 +16,8 @@ contract METActivists is ERC721A, Ownable {
     uint256 public immutable MAX_PER_WALLET = 5;
     uint256 public immutable MAX_AMOUNT = 789;
     uint256 public immutable SOFT_CAP = 700;
-    address public immutable PROXY_REGISTRY = 0x1E525EEAF261cA41b809884CBDE9DD9E1619573A;
+    address public immutable PROXY_REGISTRY =
+        0x1E525EEAF261cA41b809884CBDE9DD9E1619573A;
 
     uint256 public totalClaimed;
     mapping(address => uint256) public reserveClaims;
@@ -28,7 +28,8 @@ contract METActivists is ERC721A, Ownable {
     bool public teamClaims = false;
     uint256 public revenue;
 
-    string public baseURI_ = "https://gateway.pinata.cloud/ipfs/QmakkM1At3uxQgkUZa8xtaTkAX76nLQQbPgjJu8QZkCL2v";
+    string public baseURI_ =
+        "https://gateway.pinata.cloud/ipfs/QmakkM1At3uxQgkUZa8xtaTkAX76nLQQbPgjJu8QZkCL2v";
     bool public revealed = false;
 
     constructor() ERC721A("METActivists", "METActivists") {
@@ -39,7 +40,7 @@ contract METActivists is ERC721A, Ownable {
         contributions[0xa17FEf307070bE6e1Ef2de34A2ee110932475aB0] = 550;
         contributions[0x731ec7d7D7578CF3Bd304F7a4c49643DEc31731e] = 150;
         contributions[0xf5a5cf6eaD5BBf4f03f3F3779d0F6c1a0d841211] = 150;
-        contributions[0xc4e9ac4D7D95aA101cA8A0EabEAd26069A5dE88A] = 50;
+        contributions[0xc4e9ac4D7D95aA101cA8A0EabEAd26069A5dE88A] = 500;
         contributions[0xAbeb2d7bf8FA01a0883fe3b8c6849aff42B075B9] = 1500;
         contributions[0xe90d55fD687cE6C3601053aF4065eE5bAD4bECF7] = 1750;
     }
@@ -48,14 +49,14 @@ contract METActivists is ERC721A, Ownable {
         return 1;
     }
 
-    function claimContribution () external {
+    function claimContribution() external {
         require(getState() == 2, "Sale is not over");
         if (teamClaims == false) {
             teamClaims = true;
             revenue = address(this).balance;
         }
-        uint256 amount = contributions[msg.sender] * revenue / 10000;
-        if (amount > address(this).balance){
+        uint256 amount = (contributions[msg.sender] * revenue) / 10000;
+        if (amount > address(this).balance) {
             amount = address(this).balance;
         }
         (bool sent, ) = payable(msg.sender).call{value: amount}("");
@@ -63,39 +64,52 @@ contract METActivists is ERC721A, Ownable {
     }
 
     // states: 0="Presale", 1="Public sale", 2="Sale over"
-    function getState () public view returns(uint256 state) {
-        if(block.timestamp > END_DATE) {
+    function getState() public view returns (uint256 state) {
+        if (block.timestamp > END_DATE) {
             state = 2;
-        } else if (block.timestamp < END_DATE && block.timestamp > PUBLIC_DATE) {
+        } else if (
+            block.timestamp < END_DATE && block.timestamp > PUBLIC_DATE
+        ) {
             state = 1;
-        } else if (block.timestamp < PUBLIC_DATE && block.timestamp > PRESALE_DATE) {
+        } else if (
+            block.timestamp < PUBLIC_DATE && block.timestamp > PRESALE_DATE
+        ) {
             state = 0;
         }
     }
 
-    function reserveClaim(
-        uint256 _amount,
-        bytes32[] calldata _proof
-    ) external {
-        require(_verify(_leaf(msg.sender, _amount), rootReserve, _proof),"Invalid merkle proof");
+    function reserveClaim(uint256 _amount, bytes32[] calldata _proof) external {
+        require(
+            _verify(_leaf(msg.sender, _amount), rootReserve, _proof),
+            "Invalid merkle proof"
+        );
         require(getState() == 0, "Not my tempo");
-        require(_amount + reserveClaims[msg.sender] <= MAX_PER_WALLET, "Exceeds max allowed");
+        require(
+            _amount + reserveClaims[msg.sender] <= MAX_PER_WALLET,
+            "Exceeds max allowed"
+        );
         reserveClaims[msg.sender] += _amount;
         totalClaimed += _amount;
         _safeMint(msg.sender, _amount);
     }
 
-    function activistMint(
-        uint256 _amount,
-        bytes32[] calldata _proof
-    ) external payable{
-        require(_verify(_activistLeaf(msg.sender), rootActivist, _proof),"Invalid merkle proof");
+    function activistMint(uint256 _amount, bytes32[] calldata _proof)
+        external
+        payable
+    {
+        require(
+            _verify(_activistLeaf(msg.sender), rootActivist, _proof),
+            "Invalid merkle proof"
+        );
         require(msg.value == (_amount * ACTIVIST_PRICE), "Wrong amount.");
         require(getState() == 0, "Not my tempo");
-        require(_amount + totalActivistMint[msg.sender] <= MAX_PER_WALLET, "Exceeds max allowed");
+        require(
+            _amount + totalActivistMint[msg.sender] <= MAX_PER_WALLET,
+            "Exceeds max allowed"
+        );
         uint256 desired = totalMinted() + _amount;
-        uint256 threshold = SOFT_CAP + totalClaimed; 
-        require(desired <= threshold,"Can't grab reserved assets");
+        uint256 threshold = SOFT_CAP + totalClaimed;
+        require(desired <= threshold, "Can't grab reserved assets");
         totalActivistMint[msg.sender] += _amount;
         _safeMint(msg.sender, _amount);
     }
@@ -115,15 +129,15 @@ contract METActivists is ERC721A, Ownable {
         return keccak256(abi.encodePacked(_amount, _account));
     }
 
-    function _activistLeaf (address _account) internal pure returns (bytes32) {
+    function _activistLeaf(address _account) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(_account));
     }
 
-    function _verify(bytes32 leaf, bytes32 _root, bytes32[] memory _proof)
-        internal
-        pure
-        returns (bool)
-    {
+    function _verify(
+        bytes32 leaf,
+        bytes32 _root,
+        bytes32[] memory _proof
+    ) internal pure returns (bool) {
         return MerkleProof.verify(_proof, _root, leaf);
     }
 
@@ -134,7 +148,6 @@ contract METActivists is ERC721A, Ownable {
     function setReserveRoot(bytes32 _root) external onlyOwner {
         rootReserve = _root;
     }
-
 
     function numberMinted(address _owner) public view returns (uint256) {
         return _numberMinted(_owner);
@@ -156,22 +169,26 @@ contract METActivists is ERC721A, Ownable {
         return baseURI_;
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        if (revealed == false){
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        if (revealed == false) {
             return baseURI_;
         } else {
             return super.tokenURI(tokenId);
         }
     }
 
-
-    function reveal (string memory __baseURI) external onlyOwner {
+    function reveal(string memory __baseURI) external onlyOwner {
         require(revealed == false, "It's been revealed already.");
         baseURI_ = __baseURI;
         revealed = true;
     }
 
-    
     function exists(uint256 _tokenId) public view returns (bool) {
         return _exists(_tokenId);
     }
@@ -180,14 +197,22 @@ contract METActivists is ERC721A, Ownable {
         _burn(_tokenId, _approvalCheck);
     }
 
-    function isApprovedForAll(address _owner, address operator) public view override returns (bool) {
-        OpenSeaProxyRegistry proxyRegistry = OpenSeaProxyRegistry(PROXY_REGISTRY);
+    function isApprovedForAll(address _owner, address operator)
+        public
+        view
+        override
+        returns (bool)
+    {
+        OpenSeaProxyRegistry proxyRegistry = OpenSeaProxyRegistry(
+            PROXY_REGISTRY
+        );
         if (address(proxyRegistry.proxies(_owner)) == operator) return true;
         return super.isApprovedForAll(_owner, operator);
     }
 }
 
-contract OwnableDelegateProxy { }
+contract OwnableDelegateProxy {}
+
 contract OpenSeaProxyRegistry {
     mapping(address => OwnableDelegateProxy) public proxies;
 }
